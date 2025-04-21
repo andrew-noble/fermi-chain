9df684c0deb1290d305c06f506103a0b25de17a9
Andrew Noble <aknoble.andrew@gmail.com>
2025-04-20 18:29:13 -0700

    add great mobile/desktop popover/tooltip for oom

--- frontend/src/components/fermi-chain/display/InlineOom.tsx
+++ frontend/src/components/fermi-chain/display/InlineOom.tsx
@@ -2,16 +2,22 @@ import { Oom } from "@/types";
 import {
   Tooltip,
   TooltipContent,
-  TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+import { useIsMobile } from "@/hooks/useIsMobile";
 interface InlineOomProps {
   oom: Oom;
   className?: string;
 }
 
 export function InlineOom({ oom, className }: InlineOomProps) {
+  const isMobile = useIsMobile();
+
   const getDisplayVersion = (oom: Oom) => {
     if (oom.exponent === 0) {
       return "1";
@@ -28,20 +34,32 @@ export function InlineOom({ oom, className }: InlineOomProps) {
 
   const fullValue = oom.value.toLocaleString();
 
-  return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <span
-            className={`inline-block w-[2.5rem] text-center mr-2 ${className}`}
-          >
-            {getDisplayVersion(oom)}
-          </span>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>{fullValue}</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+  //for mobile, a touch popover, for desktop, a hover tooltip
+  return isMobile ? (
+    <Popover>
+      <PopoverTrigger asChild>
+        <span
+          className={`inline-block w-[2.5rem] text-center mr-2 ${className}`}
+        >
+          {getDisplayVersion(oom)}
+        </span>
+      </PopoverTrigger>
+      <PopoverContent className="w-fit p-2">
+        <p>{fullValue}</p>
+      </PopoverContent>
+    </Popover>
+  ) : (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <span
+          className={`inline-block w-[2.5rem] text-center mr-2 ${className}`}
+        >
+          {getDisplayVersion(oom)}
+        </span>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>{fullValue}</p>
+      </TooltipContent>
+    </Tooltip>
   );
 }
