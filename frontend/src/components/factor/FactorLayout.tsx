b229d95c58bc40b7f47821564eb3e109a06aac71
Andrew Noble <aknoble.andrew@gmail.com>
2025-04-26 09:28:27 -0700

    fix submittal constraint

--- frontend/src/components/factor/FactorLayout.tsx
+++ frontend/src/components/factor/FactorLayout.tsx
@@ -33,12 +33,14 @@ export default function FactorLayout({
   onClear,
 }: FactorLayoutProps) {
   const [numerators, denominators] = splitUnitInventory(data.unit);
-  const isValid = data.unit && Object.keys(data.unit).length > 0;
+  const isValid =
+    data.denominatorValue.fullValue !== 1 ||
+    data.numeratorValue.fullValue !== 1; //ensure user has entered something
 
   const textStyles = isEditing ? "text-base" : "text-lg md:text-xl lg:text-2xl";
 
   return isEditing ? (
-    <div className="grid grid-cols-[minmax(7rem, auto)_minmax(5rem, auto)_minmax(6rem, auto)] ring-2 ring-primary/50 p-2 rounded-md">
+    <div className="grid grid-cols-[minmax(2rem, auto)_minmax(2rem, auto)_minmax(2rem, auto)] ring-2 ring-primary/50 p-2 rounded-md">
       {/* Numerator Row */}
       <div className="col-start-1 flex items-center justify-center">
         <InlineMantissa
@@ -95,7 +97,7 @@ export default function FactorLayout({
       </div>
     </div>
   ) : (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-1 items-center">
       <FactorDisplay factor={data} textStyles={textStyles} />
       <FactorButtonGroup
         editing={isEditing}
