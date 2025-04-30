de0dda2c498934fe2cf11e9788756aadacaf3630
Andrew Noble <aknoble.andrew@gmail.com>
2025-04-29 20:58:47 -0700

    cleaning up another old file bc of deploy failure

--- frontend/src/helpers/string-formatting/index.ts
+++ frontend/src/helpers/string-formatting/index.ts
@@ -1,6 +1,5 @@
 export * from "./formatUnitToString";
 export * from "./formatNumberToString";
-export * from "./formatValueToString";
 
-//I have tried to only make strings used in copy and paste and otherwise used react components
+//I have tried to only make strings used in copy and paste and otherwise use JSX
 //This is a helper file for that
